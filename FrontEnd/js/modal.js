1c69e4c14e9b0e9c5e07ebe097b459e9471576dc
hugo566 <hugo56.lg@gmail.com>
2023-11-22 19:50:44 +0100

    Suppression de travaux existants

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -5,11 +5,10 @@ function toggleModal() {
     modalContainer.classList.toggle("active");
 }
 
-const works = fetch("http://localhost:5678/api/works").then(works => works.json());
-works.then((works) => {
-
-    // Fonction recuperation des travaux
-    function genererWorks(works) {
+// Fonction recuperation des travaux
+function genererWorks() {
+    const fetchWorks = fetch("http://localhost:5678/api/works").then(works => works.json());
+    fetchWorks.then((works) => {
         for (let i = 0; i < works.length; i++) {
             const gallery = document.querySelector("div.galleryModal");
             const figureElement = document.createElement("figure");
@@ -17,12 +16,34 @@ works.then((works) => {
             const imgElement = document.createElement("img");
             imgElement.src = works[i].imageUrl;
             imgElement.alt = works[i].title;
+            const p = document.createElement("p");
+            p.classList.add(`${works[i].id}`);
             const elementSuppr = document.createElement("i");
             elementSuppr.classList.add("fa-solid", "fa-trash-can");
+            figureElement.id = works[i].id;
+            p.appendChild(elementSuppr);
             figureElement.appendChild(imgElement);
-            figureElement.appendChild(elementSuppr);
+            figureElement.appendChild(p);
             gallery.appendChild(figureElement);
         }
-    }
-    genererWorks(works);
-})
+        const btnTrash = document.querySelectorAll("p");
+        for (let i = 0; i < btnTrash.length; i++) {
+            btnTrash[i].addEventListener("click", () => {
+                const idWorks = btnTrash[i].className;
+                const token = window.localStorage.getItem("token");
+                function deleteWorks() {
+                    fetch(`http://localhost:5678/api/works/${idWorks}`, {
+                        method: "DELETE",
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        },
+                    });
+                    const figureDelete = document.getElementById(`${idWorks}`);
+                    figureDelete.style.display = "none";
+                }
+                deleteWorks();
+            });
+        }
+    })
+}
+genererWorks();
